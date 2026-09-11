a08aa9f69a047a7d0277b37c0cb98b61c497b226
Dodogo <denislesan2022@gmail.com>
2026-09-11 08:41:03 +0300

    Task 2 este un folder cu fisiere utils si main

--- node_eb1bd6f0f2499.tmp.js
+++
@@ -1,8 +0,0 @@
-const grades = [7, 9, 5, 10, 8, 6];
-
-
-// Toate numerele mai mare sau egal cu 8
-for (let i = 0; i < grades.length; i++) {
-  if (grades[i] >= 8) {
-    console.log(grades[i]);
-  }}
